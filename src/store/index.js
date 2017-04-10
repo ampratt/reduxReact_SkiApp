b3700e1089d36e3cffb7d94ee8a65a3a791112d5
Aaron Pratt <pratt.aaron.m@gmail.com>
2017-04-10 15:10:40 +0300

    implemented action creators to handle app logic. deployed via express server. added thunk for autocomplete with isomorphic fetch.

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,27 @@
 import C from '../constants'
 import appReducer from './reducers'
+import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 
-// middleware for logging msgs to console
 const consoleMessages = store => next => action => {
 
 	let result
 
 	console.groupCollapsed(`dispatching action => ${action.type}`)
-	console.log('ski days', store.getState().allSkiDays.length);
-	
-	// dispatch the action!
+	console.log('ski days', store.getState().allSkiDays.length)
 	result = next(action)
 
 	let { allSkiDays, goal, errors, resortNames } = store.getState()
 
 	console.log(`
 
-		After action
-		============
 		ski days: ${allSkiDays.length}
 		goal: ${goal}
 		fetching: ${resortNames.fetching}
 		suggestions: ${resortNames.suggestions}
 		errors: ${errors.length}
-	
-	`);
+
+	`)
 
 	console.groupEnd()
 
@@ -34,7 +30,7 @@ const consoleMessages = store => next => action => {
 }
 
 export default (initialState={}) => {
-	return applyMiddleware(consoleMessages)(createStore)(appReducer, initialState)		//createStore(appReducer, initialState)
+	return applyMiddleware(thunk,consoleMessages)(createStore)(appReducer, initialState)
 }
 
 
